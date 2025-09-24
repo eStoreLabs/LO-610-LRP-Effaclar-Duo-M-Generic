7695d86886caeeafb4a6b74e90237fd8c01c4609
Roman Pavlenko <rpavlenko.mail@gmail.com>
2025-09-25 00:08:15 +0200

    feat: add alts

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,54 @@
-const accTab = document.querySelectorAll('#es-rc #es-rc-content .acc-tab');
+(() => {
+  const accTab = document.querySelectorAll('#es-rc #es-rc-content .acc-tab');
 
-for (let i = 0; i < accTab.length; i++) {
-  accTab[i].addEventListener('click', function () {
-    this.classList.add('active');
-    let accTabContent = this.nextElementSibling;
-
-    const hideContent = () => {
-      accTabContent.style.maxHeight = null;
-      this.classList.remove('active');
-    };
-
-    const showContent = () => {
-      accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
+  for (let i = 0; i < accTab.length; i++) {
+    accTab[i].addEventListener('click', function () {
       this.classList.add('active');
-    };
+      let accTabContent = this.nextElementSibling;
 
-    if (accTabContent.style.maxHeight) {
-      hideContent();
-    } else {
-      showContent();
-    }
+      const hideContent = () => {
+        accTabContent.style.maxHeight = null;
+        this.classList.remove('active');
+      };
+
+      const showContent = () => {
+        accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
+        this.classList.add('active');
+      };
+
+      if (accTabContent.style.maxHeight) {
+        hideContent();
+      } else {
+        showContent();
+      }
+    });
+  }
+
+  const swiper = new Swiper('.es-swiper1', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.es-swiper1-pagination',
+    },
+
+    navigation: {
+      nextEl: '.es-swiper1-button-next',
+      prevEl: '.es-swiper1-button-prev',
+    },
   });
-}
 
-const swiper = new Swiper('.es-swiper1', {
-  direction: 'horizontal',
-  loop: true,
+  const swiper2 = new Swiper('.es-swiper2', {
+    direction: 'horizontal',
+    loop: true,
 
-  pagination: {
-    el: '.es-swiper1-pagination',
-  },
+    pagination: {
+      el: '.es-swiper2-pagination',
+    },
 
-  navigation: {
-    nextEl: '.es-swiper1-button-next',
-    prevEl: '.es-swiper1-button-prev',
-  },
-
-});
-
-const swiper2 = new Swiper('.es-swiper2', {
-  direction: 'horizontal',
-  loop: true,
-
-  pagination: {
-    el: '.es-swiper2-pagination',
-  },
-
-  navigation: {
-    nextEl: '.es-swiper2-button-next',
-    prevEl: '.es-swiper2-button-prev',
-  },
-});
+    navigation: {
+      nextEl: '.es-swiper2-button-next',
+      prevEl: '.es-swiper2-button-prev',
+    },
+  });
+})();
